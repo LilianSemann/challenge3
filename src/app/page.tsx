ba68c3a2620e43414b02d2e4fe992d23e9c4c9dc
Lilian Cristine Semann <Liliansemann2525@gmail.com>
2023-10-07 20:56:22 -0300

    finally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Tasklist from "./components/tasklist";
 export default function Page() {
   return (
     <section className="min-h-screen bg-black flex justify-center items-center p-5">
-      <div className="bg-white text-gray-700 p-5 rounded-lg">
+      <div className="bg-white text-gray-700 p-5 rounded-lg min-h-[30vw] min-w-[30vw]">
         <Tasklist/>
       </div>
     </section>
